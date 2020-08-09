84c1217204fce8b11def88e0bc77bf7455a5fa87
Cryoris <jules.gacon@googlemail.com>
2020-08-09 19:16:20 +0200

    use num_parameters on x axis

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,14 @@
 import time
 import tracemalloc
 import numpy as np
-from gradients import grad, itgrad
+import matplotlib.pyplot as plt
+from gradients import StateGradient
 from qiskit.quantum_info import Statevector
 
 
 class Benchmark:
-    def __init__(self, nums_qubits, single_qubit_op, nreps):
-        self.nums_qubits = nums_qubits
+    def __init__(self, reps, single_qubit_op, nreps):
+        self.num_reps = reps
         self.single_qubit_op = single_qubit_op
         self.nreps = nreps
         self.verbose = False
@@ -21,20 +22,23 @@ class Benchmark:
         #            'itgrad': ...}
         runtime, memory = 2 * [{'grad': {'avg': {}, 'std': {}},
                                 'itgrad': {'avg': {}, 'std': {}}}]
-        for num_qubits in self.nums_qubits:
-            if self.verbose:
-                print()
-                print('num_qubits:', num_qubits)
-                print('runs:')
-            # get operator and input state of proper size
-            op = (self.single_qubit_op ^ num_qubits).to_matrix_op().primitive
-            state_in = Statevector.from_label('0' * num_qubits)
-
+        for reps in self.num_reps:
             # resize and parameterize library circuit
-            library_circuit.num_qubits = num_qubits
+            library_circuit.reps = reps
             ansatz = library_circuit.assign_parameters(
                 np.random.random(library_circuit.num_parameters)
             )
+
+            # get operator and input state of proper size
+            num_qubits = library_circuit.num_qubits
+            op = (self.single_qubit_op ^ num_qubits).to_matrix_op().primitive
+            state_in = Statevector.from_label('0' * num_qubits)
+
+            if self.verbose:
+                print()
+                print('num_qubits:', num_qubits)
+                print('reps:', reps)
+                print('runs:')
 
             # compute the average over nreps repetitions
             grad_runtimes, grad_memory_peaks = [], []
@@ -48,20 +52,21 @@ class Benchmark:
                 itgrad_runtimes.append(times[1])
                 itgrad_memory_peaks.append(peaks[1])
 
-            runtime['grad']['avg'][num_qubits] = np.mean(grad_runtimes)
-            runtime['grad']['std'][num_qubits] = np.std(grad_runtimes)
-            runtime['itgrad']['avg'][num_qubits] = np.mean(itgrad_runtimes)
-            runtime['itgrad']['std'][num_qubits] = np.std(itgrad_runtimes)
+            num_parameters = library_circuit.num_parameters
+            runtime['grad']['avg'][num_parameters] = np.mean(grad_runtimes)
+            runtime['grad']['std'][num_parameters] = np.std(grad_runtimes)
+            runtime['itgrad']['avg'][num_parameters] = np.mean(itgrad_runtimes)
+            runtime['itgrad']['std'][num_parameters] = np.std(itgrad_runtimes)
 
-            memory['grad']['avg'][num_qubits] = np.mean(grad_memory_peaks)
-            memory['grad']['std'][num_qubits] = np.std(grad_memory_peaks)
-            memory['itgrad']['avg'][num_qubits] = np.mean(itgrad_memory_peaks)
-            memory['itgrad']['std'][num_qubits] = np.std(itgrad_memory_peaks)
+            memory['grad']['avg'][num_parameters] = np.mean(grad_memory_peaks)
+            memory['grad']['std'][num_parameters] = np.std(grad_memory_peaks)
+            memory['itgrad']['avg'][num_parameters] = np.mean(itgrad_memory_peaks)
+            memory['itgrad']['std'][num_parameters] = np.std(itgrad_memory_peaks)
 
         self.last_run = {'runtime': runtime, 'memory': memory}
 
         if filename is None:
-            filename = f'n{self.nums_qubits[0]}_{self.nums_qubits[-1]}_c{library_circuit.name}'
+            filename = f'n{self.num_reps[0]}_{self.num_reps[-1]}_c{library_circuit.name}'
 
         print(self.last_run)
 
@@ -71,15 +76,48 @@ class Benchmark:
         np.save(filename, self.last_run)
 
     def load_benchmark(self, filename):
-        return np.load(filename).item()
+        return np.load(filename, allow_pickle=True).item()
+
+    def plot(self, key='runtime', filename=None):
+        if filename is None:
+            try:
+                benchmark = self.last_run
+            except NameError:
+                raise RuntimeError('Run a benchmark or pass a filename.')
+        else:
+            benchmark = self.load_benchmark(filename)
+
+        data = benchmark[key]
+        nums_parameters = list(data['grad']['avg'].keys())
+
+        colors = ['tab:blue', 'tab:orange']
+        markers = ['o', '^']
+        methods = ['grad', 'itgrad']
+
+        plt.figure()
+        plt.loglog()  # shortcut for getting log scaling on x and y axis
+        for method, color, marker in zip(methods, colors, markers):
+            avg = list(data[method]['avg'].values())
+            std = list(data[method]['std'].values())
+            plt.errorbar(nums_parameters, avg, yerr=std, color=color, marker=marker, label=method)
+
+        plt.title(key)
+        plt.legend(loc='best')
+
+        if filename is None:
+            filename = f'n{self.num_reps[0]}_{self.num_reps[-1]}_{key}.pdf'
+
+        plt.savefig('img/' + filename)
+        plt.show()
 
     @staticmethod
     def single_run(ansatz, op, state_in):
         times, peaks = [], []
-        for method in [grad, itgrad]:
+        grad = StateGradient(op, ansatz, state_in)
+        for method in ['reference_gradients', 'iterative_gradients']:
             tracemalloc.start()
             t0 = time.time()
-            _ = method(ansatz, op, state_in)
+            _ = getattr(grad, method)()  # run gradient computation
             te = time.time()
             _, peak = tracemalloc.get_traced_memory()
             tracemalloc.stop()
@@ -93,12 +131,15 @@ def efficient_su2():
     from qiskit.circuit.library import EfficientSU2
     from qiskit.aqua.operators import H
 
-    nums_qubits = [2, 4, 6, 8, 10]
+    reps = [2, 4, 6, 8]
     nreps = 10
 
-    b = Benchmark(nums_qubits, H, nreps)
+    b = Benchmark(reps, H, nreps)
     b.verbose = True
-    b.run_benchmark(EfficientSU2())
+    b.run_benchmark(EfficientSU2(5, entanglement='linear'))
+    b.plot('runtime')
+    b.plot('memory')
 
 
-efficient_su2()
+if __name__ == '__main__':
+    efficient_su2()
